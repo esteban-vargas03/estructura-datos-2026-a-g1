49cf40cca4a515817772dff80410d2efb461323e
esteban-vargas03 <estebanvargassers@gmail.com>
2026-05-23 22:12:31 -0500

    Delete 15-week/02-optional-activity directory

--- 15-week/02-optional-activity/parte-1-mi-primera-web/backend/gestion-turnos/src/main/java/com/turnos/gestion/turnos/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.turnos.gestion.turnos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends JpaRepository<Persona, Long>{
-
-}
